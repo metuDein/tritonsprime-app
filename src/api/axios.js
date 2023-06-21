bed4c0587132e99918decfe9e7b83ec284ed58cc
metudein <maxmetadein@gmail.com>
2023-06-21 10:31:06 +0100

    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3500/';
+const BASE_URL = 'https://tritonsprime-original.onrender.com';
 
 export default axios.create({
     baseURL : BASE_URL,
