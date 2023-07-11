5db59b25303a1acdeaad107a3a3b9eb5ad05509b
metudein <maxmetadein@gmail.com>
2023-07-11 20:23:46 +0100

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getStorage} from "firebase/storage"
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCEBFOMhfpUV6O-CK5tNEdQZ9RsgRFdnzE",
-  authDomain: "ricky-image-upload.firebaseapp.com",
-  projectId: "ricky-image-upload",
-  storageBucket: "ricky-image-upload.appspot.com",
-  messagingSenderId: "244331114991",
-  appId: "1:244331114991:web:da22109bab39086e9fc61a"
+  apiKey: "AIzaSyCBeAPGKXneVNxuIh-r_Yf9Zt08xK1IUAY",
+  authDomain: "nft-mage-broker.firebaseapp.com",
+  projectId: "nft-mage-broker",
+  storageBucket: "nft-mage-broker.appspot.com",
+  messagingSenderId: "825491015945",
+  appId: "1:825491015945:web:7620873309b6e9c12eebd8"
 };
 
 // Initialize Firebase
